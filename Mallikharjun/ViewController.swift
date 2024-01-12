7d575b46431e4c586352996a713ca5e3452cc316
mallikharjunaGupta <mallikharjunaGuptaP@ecssi.com>
2024-01-12 15:34:11 +0530

    initialCommit

---
+++ Mallikharjun/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mallikharjun
+//
+//  Created by Apple on 11/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var mallikharjuna: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+    
+    
+    
+}
+
